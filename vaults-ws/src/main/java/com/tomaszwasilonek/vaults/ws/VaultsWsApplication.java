925785d2d530a6ca240974c98150d142227d2868
Tomasz Wasilonek <tomasz.wasilonek@gmail.com>
2019-10-15 21:51:25 +0200

    created spring boot app and added simple user REST controller

---
+++ vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/VaultsWsApplication.java
@@ -0,0 +1,13 @@
+package com.tomaszwasilonek.vaults.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultsWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultsWsApplication.class, args);
+	}
+
+}
