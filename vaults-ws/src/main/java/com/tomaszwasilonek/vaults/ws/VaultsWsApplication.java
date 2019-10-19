146459e801c96a622f81200737a8014d2f0f98d2
Tomasz Wasilonek <tomasz.wasilonek@gmail.com>
2019-10-19 13:54:06 +0200

    added spring boot web security

--- vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/VaultsWsApplication.java
+++ vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/VaultsWsApplication.java
@@ -2,12 +2,19 @@ package com.tomaszwasilonek.vaults.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class VaultsWsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaultsWsApplication.class, args);
+	}
+	
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
--- vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/service/UserService.java
+++ vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/service/UserService.java
@@ -1,8 +1,10 @@
 package com.tomaszwasilonek.vaults.ws.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.tomaszwasilonek.vaults.ws.shared.dto.UserDto;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
 	
 	UserDto createUser(UserDto user);
 }
