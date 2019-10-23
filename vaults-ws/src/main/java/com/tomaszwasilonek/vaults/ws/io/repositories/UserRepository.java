e4a9eebbd19222313f8ee04863565df240a3f10c
Tomasz Wasilonek <tomasz.wasilonek@gmail.com>
2019-10-23 22:29:57 +0200

    VLTS-9: added get users collection

--- vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/io/repositories/UserRepository.java
+++ vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/io/repositories/UserRepository.java
@@ -1,12 +1,12 @@
 package com.tomaszwasilonek.vaults.ws.io.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 import com.tomaszwasilonek.vaults.ws.io.entity.UserEntity;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, Long> {
+public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
 	UserEntity findByEmail(String email);
 	UserEntity findByUserId(String userId);
 }
--- vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/service/UserService.java
+++ vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/service/UserService.java
@@ -1,5 +1,7 @@
 package com.tomaszwasilonek.vaults.ws.service;
 
+import java.util.List;
+
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import com.tomaszwasilonek.vaults.ws.shared.dto.UserDto;
@@ -13,6 +15,8 @@ public interface UserService extends UserDetailsService {
 	UserDto getUser(String email);
 	
 	UserDto getUserByUserId(String userId);
+	
+	List<UserDto> getUsers(int page, int limit);
 	
 	void deleteUser(String userId);
 	
