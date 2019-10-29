a2d8f56bee1058bd2af95c0e7dc72b1b944893a7
Tomasz Wasilonek <tomasz.wasilonek@gmail.com>
2019-10-29 21:48:07 +0100

    changed target app from .jar to .war

--- vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/VaultsWsApplication.java
+++ vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/VaultsWsApplication.java
@@ -2,13 +2,21 @@ package com.tomaszwasilonek.vaults.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.tomaszwasilonek.vaults.ws.security.AppProperties;
 
 @SpringBootApplication
-public class VaultsWsApplication {
+public class VaultsWsApplication extends SpringBootServletInitializer {
+	
+	// Needed to create a WAR file
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(VaultsWsApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VaultsWsApplication.class, args);
