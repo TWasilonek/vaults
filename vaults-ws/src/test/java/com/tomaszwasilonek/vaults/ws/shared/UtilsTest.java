56e7aefd3b36c52edeb52adf0f78e2bb7c7fac75
Tomasz Wasilonek <tomasz.wasilonek@gmail.com>
2020-01-02 17:40:42 +0100

    added support for H2 in memory DB

---
+++ vaults-ws/src/test/java/com/tomaszwasilonek/vaults/ws/shared/UtilsTest.java
@@ -0,0 +1,33 @@
+package com.tomaszwasilonek.vaults.ws.shared;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilsTest {
+	
+	@Autowired
+	Utils utils;
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testGenerateUserId() {
+		String userId = utils.generateUserId(30);
+		String userId2 = utils.generateUserId(30);
+		
+		assertNotNull(userId);
+		assertNotNull(userId2);
+		
+		assertTrue(userId.length() == 30);
+		assertTrue(!userId.equalsIgnoreCase(userId2));
+	}
+
+}
