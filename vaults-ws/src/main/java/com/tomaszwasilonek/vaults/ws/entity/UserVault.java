de4f6d159bb893300702cd6413c07eb959d27f41
Tomasz Wasilonek <tomasz.wasilonek@gmail.com>
2020-02-16 19:10:43 +0100

    added Lombok annotations to dto and entities

--- vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/entity/UserVault.java
+++ vaults-ws/src/main/java/com/tomaszwasilonek/vaults/ws/entity/UserVault.java
@@ -10,7 +10,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import lombok.Data;
+
 @Entity
+@Data
 public class UserVault implements Serializable {
 
 	private static final long serialVersionUID = 2278145604595676191L;
@@ -31,47 +34,5 @@ public class UserVault implements Serializable {
 	@ManyToOne
 	@JoinColumn(name="users_id")
 	private UserEntity userDetails;
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getVaultId() {
-		return vaultId;
-	}
-
-	public void setVaultId(String vaultId) {
-		this.vaultId = vaultId;
-	}
-
-	public UserEntity getUserDetails() {
-		return userDetails;
-	}
-
-	public void setUserDetails(UserEntity userDetails) {
-		this.userDetails = userDetails;
-	}
-
-	public double getBalance() {
-		return balance;
-	}
-
-	public void setBalance(double balance) {
-		this.balance = balance;
-	}
-	
-	
 
 }
